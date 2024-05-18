c8c953689e22926e408d7f2e7cb59a40f1b37f3f
amirreza <amirreza@fimo.io>
2024-05-18 14:40:50 +0330

    add env address

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/amirashouri/portfolio
 
 go 1.22.1
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my portfolio")
 	})
 
-	err := http.ListenAndServe(":8080", nil)
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	err := http.ListenAndServe(listenAddr, nil)
 	if err != nil {
 		panic("Server was shot down")
 	}
