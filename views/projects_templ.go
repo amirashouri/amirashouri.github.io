53a6295063bee899e2d3eec450204859df8583f4
amirreza <amirreza@fimo.io>
2024-06-08 16:36:07 +0330

    impl projects. add github action yaml

--- views/projects_templ.go
+++ views/projects_templ.go
@@ -10,6 +10,8 @@ import "context"
 import "io"
 import "bytes"
 
+import "portfolio/views/components"
+
 func Projects() templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
@@ -23,7 +25,31 @@ func Projects() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"container text-primary-600\"><p class=\"\">Projects</p></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"grid grid-cols-1 gap-4 place-content-center justify-items-center py-4\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = components.ProjectItem("Senior Mobile Engineer at FimoHealth", "Fimo Health is a leading company in health companionship based in Cologne-Germany. I started my journey as a senior iOS Developer to improve, maintain and redesign the native iOS app. Currently i'm working on the second FimoHealth mobile app written in Flutter and dart.", "since Nov 2022").Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = components.ProjectItem("Senior iOS Developer at Digikala", "Digikala is the biggest e-commerce platform based in Iran, i joined Digikala as a senior iOS Developer to redesign and modularize the existing project. I faced a lot of new challenges which was the result of working in an intense and professional environment.", "Aug 2021 - Nov 2022").Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = components.ProjectItem("iOS Developer at #780", "#780 is a payment and top-up service company. I started working there as an iOS developer my first experience working in a big team on an app with a couple of million users.", "Jan 2019 - Aug 2021").Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = components.ProjectItem("iOS Developer Wikitor", "Wikitor was an educational startup trying to connect student and teacher via two separate applications. I was in charge of developing and designing two iOS apps from scratch.", "May 2017 - Jan 2019").Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = components.ProjectItem("iOS Developer Web24", "Web24 is a Search Engine Optimization(SEO) company. Despite being a seo company they also accept different client projects. This was my first official position as a developer working on multiple client apps.", "Jul 2016 - May 2017").Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
